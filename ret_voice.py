fedee6e282aa304aa7e41425a72b7e32d3b26b00
SodaLee <sodalee@pku.edu.cn>
2018-04-02 18:55:55 +0800

    final

--- ret_voice.py
+++ ret_voice.py
@@ -38,37 +38,29 @@ def find_dish(dish_name):
 	return -1
 
 #request_type: 0: new dish, 1: next step, 2: repeat
-def ret_voice(dish_name, is_new, user_id, request_type):
+def ret_voice(dish_name, user_id, request_type):
 	global users
-	step_id = 0
-	if is_new:
-		new_user = dict()
-		dish_id = find_dish(dish_name)
-		new_user['dish_id'] = dish_id
-		new_user['step_id'] = 0
-		new_user['time_stamp'] = time.time()
-		ret = server_dish(dish_id, 0)
-		users[user_id] = new_user
-		if dish_id == -1:
-			return -1, "我还不会这个菜哦？", 0
-		if request_type != 0:
-			return -2, "请问要做什么菜？", 0
+	info = find_user(user_id)
+	if info == 0:
+		return -2, "您的菜已经做完啦！请问要做什么菜？", 0
+	info['time_stamp'] = time.time()
+	dish_id = info['dish_id']
+	if dish_id == -1 and request_type != 0:
+		return -1, "我还不会这个菜哦？", 0
+	step_id = info['step_id']
+	if request_type == 1:
+		step_id += 1
+		ret = server_dish(dish_id, step_id)
+		info['step_id'] = step_id
+	elif request_type == 2:
+		ret = server_dish(dish_id, step_id)
 	else:
-		info = find_user(user_id)
-		if info == 0:
-			return -2, "您的菜已经做完啦！请问要做什么菜？", 0
-		info['time_stamp'] = time.time()
-		dish_id = info['dish_id']
-		if dish_id == -1:
-			return -1, "我还不会这个菜哦？", 0
-		step_id = info['step_id']
-		if request_type == 1:
-			step_id += 1
-			ret = server_dish(dish_id, step_id)
-			info['step_id'] = step_id
-		else:
-			ret = server_dish(dish_id, step_id)
-		users[user_id] = info
+		dish_id = find_dish(dish_name)
+		info['dish_id'] = dish_id
+		info['step_id'] = 0
+		step_id = 0
+		ret = server_dish(dish_id, 0)
+	users[user_id] = info
 	if ret[0] == -1:
 		del users[user_id]
 	return step_id, ret[0], ret[1]
@@ -118,8 +110,8 @@ def load_data():
 	cuisine = [dish.split('\n#\n') for dish in t]
 	cuisine = list(map(extract, cuisine))
 
-def select(request_type, dish_name, is_new, user_id):
-	step_id, words, time = ret_voice(dish_name, is_new, user_id, request_type)
+def select(request_type, dish_name, user_id):
+	step_id, words, time = ret_voice(dish_name, user_id, request_type)
 	save_user()
 	return ret_list(step_id, words, time)
 
