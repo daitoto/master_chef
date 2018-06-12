7f3800d8105786e1f7ec22f83607bb90cbc2cf72
DaiToto <1500012781@pku.edu.cn>
2018-06-12 21:42:15 +0800

    not

--- user.py
+++ user.py
@@ -3,20 +3,24 @@ import pickle
 users = dict()
 
 #search user info by id
-def find_user(user_id):
+def find_user():
 	global users
 	f_user = open("data/users.pkl", "rb")
 	users = pickle.load(f_user)
 	f_user.close()
-	try:
-		return users[user_id]
-	except:
-		users[user_id] = {'dish_id': -1, 'step_id': 0, 'time_stamp': 0}
-		return users[user_id]
+	print(len(users))
+	print(users)
+	# try:
+	# 	return users[user_id]
+	# except:
+	# 	users[user_id] = {'dish_id': -1, 'step_id': 0, 'time_stamp': 0}
+	# 	return users[user_id]
 
 #save user info
-def save_user():
-	global users
-	f_user = open("data/users.pkl", "wb")
-	pickle.dump(users, f_user)
-	f_user.close()
+# def save_user():
+# 	global users
+# 	f_user = open("data/users.pkl", "wb")
+# 	pickle.dump(users, f_user)
+# 	f_user.close()
+
+find_user()
