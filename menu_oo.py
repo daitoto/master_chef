4f23c2a2b881c2472bef0458dd9a226d3ccb671c
DaiToto <1500012781@pku.edu.cn>
2018-06-15 22:28:55 +0800

    del ad

--- menu_oo.py
+++ menu_oo.py
@@ -94,10 +94,10 @@ class Response(object):
 		ret = ""
 		for w in words:
 			if w in self.keywords:
-				if random.random() <= 1:
-					ret += self.keywords[w][1]
-				else:
-					ret += self.keywords[w][0]
+				# if random.random() <= 1:
+				# 	ret += self.keywords[w][1]
+				# else:
+				ret += self.keywords[w][0]
 			else:
 				ret += w
 		return ret
@@ -105,7 +105,7 @@ class Response(object):
 	def _find_ads(self, tim):
 		ret = []
 		time_left = tim
-		voice_ad = False
+		voice_ad = True
 		flag = 1
 		random.shuffle(self.ads)
 		while time_left and not voice_ad:
